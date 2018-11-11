3d02a4494c07e7586be178df7401d61523fac2b3
benjamin-summers <summerstyler2@gmail.com>
2018-11-11 00:15:38 -0500

    app working

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -3,6 +3,9 @@ import { connect } from 'react-redux';
 
 class BookDetail extends Component {
     render() {
+        if(!this.props.book) {
+            return <div>Select a book to get started.</div>;
+        }
         return (
             <div>
                 <h3>Details for:</h3>
@@ -13,9 +16,9 @@ class BookDetail extends Component {
 }
 
 function mapStateToProps(state) {
-    return (
+    return {
         book: state.activeBook
-    )
+    }
 }
 
 export default connect(mapStateToProps)(BookDetail);
