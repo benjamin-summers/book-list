36ed9253c3de870b8f29549489ecb6b192f663a0
benjamin-summers <summerstyler2@gmail.com>
2018-11-11 00:48:21 -0500

    app working

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class BookDetail extends Component {
+    bookImageStyle = {
+        height: '100' + 'px',
+        width: '100' + 'px',
+    }
+
+    bookDetailStyle = {
+        textAlign: 'center'
+    }
     render() {
         if(!this.props.book) {
             return <div>Select a book to get started.</div>;
         }
         return (
-            <div>
+            <div style={this.bookDetailStyle}>
                 <h3>Details for:</h3>
                 <div>{this.props.book.title}</div>
+                <div>
+                    <img style={this.bookImageStyle} src={this.props.book.src} alt={this.props.book.title + ' image'}/>
+                </div>
+                <hr />
+                <h5># Of Pages {this.props.book.pages}</h5>
+                <hr />
+                <h5>Read more</h5>
             </div>
         );
     }
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
  export default function() {
      return [
-         { title: 'Javascript: The Good Parts' },
-         { title: 'Harry Potter' },
-         { title: 'The Dark Tower' },
-         { title: 'Eloquent Ruby' }
+         { title: 'Javascript: The Good Parts', pages: '80', src: 'https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX381_BO1,204,203,200_.jpg' },
+         { title: 'Harry Potter', pages: '400', src: 'https://www.scholastic.com/content5/media/products/71/9780439358071_mres.jpg'  },
+         { title: 'The Dark Tower', pages: '315', src: 'https://prodimage.images-bn.com/pimages/9781416524526_p0_v3_s550x406.jpg'  },
+         { title: 'Eloquent Ruby', pages: '250', src: 'https://images-na.ssl-images-amazon.com/images/I/414gR4dCXiL._SX387_BO1,204,203,200_.jpg'  }
      ]
  }
